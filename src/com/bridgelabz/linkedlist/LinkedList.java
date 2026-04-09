c3b06543177b08747b4f9b62f76a0a8c98dcae91
BL-JyothsnaThota <jyothsna.thota@bridgelabz.com>
2026-04-09 15:54:32 +0530

    Ability to insert at middle after some value

--- src/com/bridgelabz/linkedlist/LinkedList.java
+++ src/com/bridgelabz/linkedlist/LinkedList.java
@@ -3,29 +3,45 @@ public class LinkedList<T> {
 
     Node<T> head;
 
-    // UC3 Append Method (Insert at End)
+    // UC3 Append (reuse)
     public void append(T data) {
-
         Node<T> newNode = new Node<>(data);
 
-        // If list is empty
         if (head == null) {
             head = newNode;
             return;
         }
 
-        // Traverse to last node
         Node<T> temp = head;
         while (temp.next != null) {
             temp = temp.next;
         }
 
-        // Add new node at end
         temp.next = newNode;
     }
 
-    public void printList() {
+    // UC4 Insert After Given Value
+    public void insertAfter(T prevData, T newData) {
 
+        Node<T> temp = head;
+
+        // Search for node with prevData
+        while (temp != null && !temp.data.equals(prevData)) {
+            temp = temp.next;
+        }
+
+        // If node found
+        if (temp != null) {
+            Node<T> newNode = new Node<>(newData);
+
+            newNode.next = temp.next;
+            temp.next = newNode;
+        } else {
+            System.out.println("Value " + prevData + " not found");
+        }
+    }
+
+    public void printList() {
         Node<T> temp = head;
 
         while (temp != null) {
--- src/com/bridgelabz/linkedlist/LinkedListMain.java
+++ src/com/bridgelabz/linkedlist/LinkedListMain.java
@@ -6,8 +6,10 @@ public class LinkedListMain {
         LinkedList<Integer> list = new LinkedList<>();
 
         list.append(56);
-        list.append(30);
         list.append(70);
+
+        // Insert 30 after 56
+        list.insertAfter(56, 30);
 
         list.printList();
     }
